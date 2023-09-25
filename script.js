489797424a45ecf7c8363dec9d08a01e4c25aaaf
Test User <ahnaf.shakil24@gmail.com>
2023-09-25 13:21:48 +0600

    ad

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const canvas =document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH =canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEmemies = 20;
+const numberOfEmemies = 100;
 const enemiesArray = [];    
 
 let gameFrame = 0;
@@ -16,20 +16,26 @@ class Enemy {
         this.speed = Math.random() * 4 + 1;
         this.spriteWidth = 218;
         this.spriteHeight = 177;
-        this.width = this.spriteWidth /  2.5;
-        this.height = this.spriteHeight / 2.5;
+        this.width = this.spriteWidth /  2;
+        this.height = this.spriteHeight / 2;
         this.x = Math.random() * (canvas.width - this.width);
         this.y = Math.random() * (canvas.height - this.height);
+        this.newX = Math.random() * (canvas.width - this.width);
+        this.newY = Math.random() * (canvas.height - this.height);
         this.frame = 0;
         this.flapSpeed = Math.floor(Math.random() * 3 +  1);
-        this.angle = 0;
-        this.angleSpeed = Math.random() * 0.2;
-        this.curve = Math.random() * 7;
+        this.interval =Math.floor(Math.random() * 200 + 50);
+    
     }
-    update(){
-        this.x -= Math.sin(this.angle * Math.PI/180);
-        this.y += this.curve * Math.sin(this.angle);
-        this.angle += this.angleSpeed;
+    update(){ 
+        if (gameFrame % this.interval === 0){
+            this.newX = Math.random() * (canvas.width - this.width);
+            this.newY = Math.random() * (canvas.height - this.height);
+        }
+       let dx = this.x -this.newX;
+       let dy = this.y - this.newY;
+       this.x -= dx/70;
+       this.y -= dy/70;
         if (this.x + this.width < 0) this.x = canvas.width;
         //animate sprite
         if (gameFrame % 2 === 0){
